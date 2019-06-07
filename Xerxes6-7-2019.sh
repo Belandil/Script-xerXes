68424bf5c674906b8045634c8b1b0c9933486274
Belandil <24280249+Belandil@users.noreply.github.com>
2019-06-07 16:00:50 +0200

    Add files via upload

---
+++ Xerxes6-7-2019.sh
@@ -0,0 +1,183 @@
+#!/usr/bin/bash
+#***********************************************************************|
+# Project           : Vulnhub VM scanner							    |
+# Program name      : xerXes.sh 										|
+# Author            : x8el4nd1l											|
+# Date created      : 20190509											|	
+# Purpose           : Scans differents services on the definned target 	|
+#					  whith its IP and Machine Name in parameter. 		|
+#					  Prepare priv-Esc thanks to OS vulnerability search|
+# 																		|
+# Revision History  :													|
+# Date        Author      Ref    Details info						 	|
+# 20190509    x8el4nd1l    1     First part is reliable :				|
+#								- Nmap + Searchsploit OS + Kernel		|
+#  								- Gobuster depending on nmap services   |
+#												 						|
+#																		|
+#***********************************************************************|
+
+set -euo pipefail
+IFS=$'\n\t'
+ip=${1:-}
+machineName=${2:-}_autoXerxes
+FolderName=${2:-}_autoXerxes
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+ORANGE='\033[0;33m'
+ENDCOLOR='\e[0m'
+
+if [ -n "$1" ] && [ -n "$2" ]; then
+	echo -e "${GREEN}Variables seems to be seted, let's keep going the script ... ${ENDCOLOR}"
+else
+	echo -e "${RED}Variables seems to NOT be seted, Script will going wrong ${ENDCOLOR}"
+	exit 1
+fi
+
+pathName=~/Desktop/vm-vulnhub/"$FolderName"
+mkdir -p $pathName
+echo "Variables Created"
+a=$( touch $pathName/info${machineName}.txt )
+b=$( touch ${pathName}/flaw${machineName}.txt )
+c=$( touch $pathName/gobuster${machineName}.txt )
+d=$( touch $pathName/nmap${machineName}.txt )
+echo "Empty files Created"
+
+echo -e "${GREEN}Creating Files in directory${ENDCOLOR}"
+ls -altr $pathName
+###########################
+### NMAP AREA Starts
+###########################
+echo "****nmap starting********"
+nmap -A -T5 -v -p- -sS "$ip" 1> "$pathName"/nmap"$machineName".txt
+ls -al "$pathName"/nmap"$machineName".txt
+echo "****nmap Finished*******"
+echo -e "\n\nYour new Nmap File TCP :"
+ls -al "$pathName"/nmap"$machineName".txt
+echo -e "\n"
+
+### Comment for next line : grep lines becoming by P || O || U || 0-9|| _ || | then sed OS CPE + Uptime until last line (useless lines) + replace random spaces between words by 1 space 
+egrep '^[POU]|^[0-9]|^\||^\_' "$pathName"/nmap"$machineName".txt | sed '/OS CPE:/d; /Uptime guess:/,$d' | sed -e "s/[[:space:]]\+/ /g" > "$pathName"/flaw"$machineName".txt
+cat "$pathName"/flaw"$machineName".txt
+
+OS_VERSION=$( egrep '^OS details:' "$pathName"/flaw"$machineName".txt | cut -d ' ' -f 3-4 )
+echo "Searchsploit with the OS VERSION: $OS_VERSION"
+searchsploit "$OS_VERSION"
+
+
+serverHeader=$(grep -E 'http-server-header:' "$pathName"/flaw"$machineName".txt | cut -d ' ' -f 2 | sed -e 's/\// /g' | sort -u)
+echo "Searchsploit with the server(s) Header(s): $serverHeader"
+serverHeaderLines=$(grep -E 'http-server-header:' "$pathName"/flaw"$machineName".txt | cut -d ' ' -f 2 | sed -e 's/\// /g' | sort -u | wc -l)
+
+i=1
+while [ "$i" -le "$serverHeaderLines" ]; do
+	echo " This is i : $i"
+	serverHeaderLineShift=$(grep -E 'http-server-header:' "$pathName"/flaw"$machineName".txt | cut -d ' ' -f 2 | sed -e 's/\// /g' | sort -u | sed -n "$i"p)
+	echo "serverHeaderLineShift : $serverHeaderLineShift"
+	sleep 5s
+	if [ -n serverHeaderLineShift ]; then
+		searchsploit -e "$serverHeaderLineShift"
+		i=$[$i+1]
+	else
+		echo " NUUUUUUL"
+	fi
+done
+i=0
+
+echo " NMAP AREA FINISHED "
+
+###########################
+### NMAP AREA Finished
+###########################
+
+echo "Before numberLines"
+numberLines=$(grep -E 'open http|open ssl/http' "$pathName"/flaw"$machineName".txt  | cut -d "/" -f 1 | wc -l)
+echo "This is numberLines: $numberLines"
+webPortsOpen=$(grep -E 'open http|open ssl/http' "$pathName"/flaw"$machineName".txt  | cut -d "/" -f 1 | tr '\n' ' ')
+echo -e "return value: $? webPortsOpen value : $webPortsOpen numberLines value: $numberLines"
+
+echo -e "\nThis is webPortsOpen: "$webPortsOpen
+echo -e "\n\nThis is number of http|open Lines : $numberLines ; This is webPortsOpen: $webPortsOpen and this is retrurn $?"
+
+echo "$pathName"/flaw"$machineName".txt
+
+echo "GREP FINISHED"
+i=1 #used for browse each http services lines in flaw file
+j=1 #used for grab each directories lines in the following file : "$pathName"/gobuster"$machineName"-port"$q".txt
+########### grabing HTTP or HTTPS services and checks directories
+while [ "$i" -le "$numberLines" ]; do
+	q=$(grep -E 'open http|open ssl/http' "$pathName"/flaw"$machineName".txt | cut -d "/" -f 1 | head -n "$i" | tail -n1)
+	singleWebPort=$(echo This is q: "$q")
+	echo "Single port is : $singleWebPort"
+	echo  "Trying on : http://$ip:$q"
+	gobuster -u "http://$ip:$q" -w /usr/share/dirbuster/wordlists/directory-list-2.3-small.txt > "$pathName"/gobuster"$machineName".txt
+	echo -e "\v\v" >> "$pathName"/gobuster"$machineName".txt #This is used to carriage return after each gobuster enumeration
+	awk 'BEGIN{RS=ORS="\n\n";FS=OFS="\n"}/'$ip:$q'/' "$pathName"/gobuster"$machineName".txt | grep -E '^/' | sort -k 3,3 >> "$pathName"/gobuster"$machineName"-port"$q".txt # This line will write in file all URL repositories found in gobuster's scan, sort by code status
+	directroriesTotalLines=$(awk 'BEGIN{RS=ORS="\n\n";FS=OFS="\n"}/'$ip:$q'/' "$pathName"/gobuster"$machineName".txt | grep -E '^/' | sort -k 3,3 | wc -l) #same line than upward, returns number of lines
+	echo -e "\n number of directories lines: $directroriesTotalLines"
+	
+	echo -e "\n These are directroriesTotalLines : $directroriesTotalLines and this is j : $j"
+	#sleep 5s
+	while [ "$j" -le "$directroriesTotalLines" ]; do
+		#j=1
+		echo "Debut j value : $j"
+		getLines=$(grep -E '^/' "$pathName"/gobuster"$machineName"-port"$q".txt | sort -k 3,3 | awk '{print $1}' | head -n "$j" | tail -n 1) #browse inside directories file and get each lines depending on j value
+		echo -e "\n The value of getLines is : $getLines"
+		sleep 2s
+		firefox -new-tab "http://$ip:$q$getLines"&
+		j=$[$j+1]
+		echo "Fin j value : $j"
+	done
+	i=$[$i+1]
+done
+
+
+
+i=0 # reset i for other incrementation
+cat "$pathName"/gobuster"$machineName".txt
+
+###########Gobuster is finished, now check if there is a wordpress from Gobuster's results ########### 
+
+portsGobuster=$(grep Url/Domain "$pathName"/gobuster"$machineName".txt | cut -d ':' -f 4 | cut -d '/' -f 1 | sort -u)
+portGobusterLines=$(grep Url/Domain "$pathName"/gobuster"$machineName".txt | cut -d ':' -f 4 | cut -d '/' -f 1 | sort -u | wc -l)
+
+<< Comment
+i=1
+while [ "$i" -le "$portGobusterLines" ]; do
+	echo " This is i : $i"
+	portGobusterLineShift=$(grep Url/Domain "$pathName"/gobuster"$machineName".txt | cut -d ':' -f 4 | cut -d '/' -f 1 | sort -u | sed -n "$i"p)
+	echo "portGobusterLineShift : $portGobusterLineShift"
+	sleep 5s
+	if [ -n serverHeaderLineShift ]; then
+		echo "if loop $portGobusterLineShift"
+		i=$[$i+1]
+	else
+		echo " NUUUUUUL"
+	fi
+done
+i=0
+
+Comment
+
+echo -e "\nThis is the end"
+
+#set +e # Needed bcause grep could not get a wp word
+#grep -E 'wordpress|wp' "$pathName"/gobuster"$machineName"
+#valueBackWP=$( echo "$?" )
+
+#joom=$(grep Joomla nmapRTB2_autoXerxes.txt | cut -d ' ' -f 2-3)
+#valueBackJOOMLA=$( echo "$?" )
+#searchsploit -e $joom
+#set -e
+#echo "$valueBack"
+#
+
+#if [ "$valueBack" -ge "1" ]; then
+#	echo -e "There is no worpress or wp folder, grep error : ${valueBack}"
+#elif [ "$valueBack" -eq "0" ]; then
+#	echo -e "There is a wordpress or wp directory, grep returns : ${valueBack}"
+#else 
+#	echo -e "Fatal error grep returns error : ${valueBack}"
+#fi
